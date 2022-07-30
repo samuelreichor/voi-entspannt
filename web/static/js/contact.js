a31c4c982851d6809701d0cc8263fe90b987cfbd
samuelreichor <samuelreichor@gmail.com>
2022-07-30 16:45:53 +0200

    Contact-Form changed

--- web/static/js/contact.js
+++ web/static/js/contact.js
@@ -5,7 +5,7 @@ window.onload = function() {
         // generate a five digit number for the contact_number variable
 
         // these IDs from the previous steps
-        emailjs.sendForm('service_ahd6yr7', 'template_jli0ypm', this)
+        emailjs.sendForm('service_4x8m01b', 'template_k2v78yb', this)
             .then(function() {
                 console.log('SUCCESS!');
                 document.getElementById("contact-form").reset();
