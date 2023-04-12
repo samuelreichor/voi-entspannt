887af6b50548eb549c3f135de5d7fb7811e4919a
samuelreichor <samuelreichor@gmail.com>
2023-04-12 20:52:54 +0200

    Add some quick fixes

--- web/static/js/navbar.js
+++ web/static/js/navbar.js
@@ -1,9 +1,10 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const header = document.querySelector('header');
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
-  
+
 })
 
 toggleButton.addEventListener('click', () => {
